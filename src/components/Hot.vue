b2accea0260178e8c75ed0922fb1f8d50a6d85c3
unknown <liangrumeng@ylcaifu.com>
2019-03-26 17:51:53 +0800

    热点页面

--- src/components/Hot.vue
+++ src/components/Hot.vue
@@ -1,19 +1,46 @@
 <template>
     <div class="hot-news">
         <ul>
-            <li class="item">
+            <!-- 左右布局 -->
+            <li class="item1">
                 <div class="left-txt">
                     <span>美国唯一的空降部队-陆军第82空降师</span>
                     <div class="bottom-part">
                         <div>
-                            <img src="">
-                            <span>人民日报海外站</span>
+                            <img src="../assets/hot/hot.png">
+                            <span>人民日报海外站<i>100评</i></span>
                         </div>
                         <span></span>
                     </div>
                 </div>
                 <div class="right-img">
-                    <img src="https://p3.pstatp.com/list/pgc-image/38cb52aab2b749ab9f9c354480f8c531">
+                    <img class="imgW" src="https://p3.pstatp.com/list/pgc-image/38cb52aab2b749ab9f9c354480f8c531">
+                </div>
+            </li>
+            <li class="item1">
+                <div class="left-txt">
+                    <span>美国唯一的空降部队-陆军第82空降师</span>
+                    <div class="bottom-part">
+                        <div>
+                            <img src="../assets/hot/hot.png">
+                            <span>人民日报海外站<i>100评</i></span>
+                        </div>
+                        <span></span>
+                    </div>
+                </div>
+                <div class="right-img">
+                    <img class="imgW" src="https://p3.pstatp.com/list/pgc-image/38cb52aab2b749ab9f9c354480f8c531">
+                </div>
+            </li>
+            <!-- 上下布局 -->
+            <li class="item2">
+                <div class="top-txt">
+                    谁说女子不如男！中国第一女司机，24岁轻松开动6万吨航母
+                </div>
+                <div class="img-three">
+                    <img class="imgW" src="https://p3.pstatp.com/list/pgc-image/fb6c1e8a57074998a6fcd09dc43e9237">
+                    <img class="imgW" src="https://p3.pstatp.com/list/pgc-image/fb6c1e8a57074998a6fcd09dc43e9237">
+                    <img class="imgW" src="https://p3.pstatp.com/list/pgc-image/fb6c1e8a57074998a6fcd09dc43e9237">
                 </div>
             </li>
         </ul>
@@ -30,23 +57,45 @@ export default {
 *{
     margin:0;
     padding:0;
+    list-style: none;
 }
 .hot-news{
     padding:0 15px;
 }
-.item{
+.item1{
     display:flex;
+    margin-bottom:10px;
 }
-.item .left-txt{
+.item1 .left-txt{
     flex:8;
 }
-.item .right-img{
+.item1 .right-img{
     flex:4;
     text-align: right;
 }
-.right-img img{
+img.imgW{
     width:100px;
     height: 65px;
 }
+.bottom-part img{
+    width:16px;
+}
+.bottom-part span{
+    color:#999;
+    font-size: 12px;
+}
+.bottom-part span i{
+    font-style: normal;
+    margin-left: 5px;
+}
+.item2 .top-txt{
+    line-height: 30px;
+}
+.img-three{
+    text-align: center;
+}
+
 </style>
+
+
 
